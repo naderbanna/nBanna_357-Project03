f0480fd9b18e84d9fae9874412c19ea66431146e
naderbanna <>
2021-03-15 14:11:15 -0700

    created xcode project

---
+++ nBanna_Project03/nBanna_Project03/nBanna_Project03App.swift
@@ -0,0 +1,17 @@
+//
+//  nBanna_Project03App.swift
+//  nBanna_Project03
+//
+//  Created by Nader Banna on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct nBanna_Project03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
